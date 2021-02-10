e6cbb7dd0f502070f5ca0c72c202331b2db8fa78
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2021-02-10 11:57:57 +0200

    Add random number to the end of image link
    On refresh, a new number is appended to the end of the link, fetching a new image

--- Quote-Background.js
+++ Quote-Background.js
@@ -9,7 +9,7 @@
                 based on the users screen size */
 const getBackground = () => {
   let imgURL = "https://source.unsplash.com/random/" + $(window).width()
-    + "x" + $(window).height()
+    + "x" + $(window).height() + "?random=" + Math.round(Math.random() * 1000);
   $(".background").css("background-image", "url(" + imgURL + ")");
 }
                 /*Gets a random quote, then modifies the HTML to display it.
@@ -17,25 +17,28 @@ const getBackground = () => {
                   have the quote pre-typed when the link is opened. */
 const getQuote = () => {
   $.ajax({
+    type: "GET",
+    dataType: "json",
     headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
       Accept: "application/json",
       "Content-Type": "application/x-www-form-urlencoded"
     },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
     success: function(val) {
-      console.log(val);
       if (typeof val === "string") {
         val = JSON.parse(val);
       }
-      $(".quote__text--quote").html(val.quote);
-      $(".quote__text--author").html(" - " + val.author);
-      let quoteForLink = encodeURIComponent(val.quote).replace(/'/g, "%27");
-      let authorForLink = encodeURIComponent(val.author).replace(/'/g, "%27");
+      $(".quote__text--quote").html(val.quoteText);
+      $(".quote__text--author").html(" - " + val.quoteAuthor);
+      let quoteForLink = encodeURIComponent(val.quoteText).replace(/'/g, "%27");
+      let authorForLink = encodeURIComponent(val.quoteAuthor).replace(/'/g, "%27");
       $(".button--tweet").html("<a href='https://twitter.com/intent/tweet?text="
         + window.quoteForLink +"  - "+ window.authorForLink
         + "'><span class='glyphicon glyphicon-send'></span>    Tweet</a>");
       },
+    error: function(errorMessage) {
+        alert("Error");
+    },
     });
 }
 
@@ -43,6 +46,8 @@ $(document).ready(() => {
   getBackground();
   getQuote();
   $(".button--generate").click(function () {
-    location.reload(); //reloads the page for a new tweet/background
+    // workaround for codepen because it doesn't like location.reload()
+    getBackground();
+    getQuote();
   });
 });
